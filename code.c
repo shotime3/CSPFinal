f3a4fb49d2ec2ab962bb6d67d2208eaa2cfa7421
shotime3 <shohuang85@gmail.com>
2019-06-09 22:40:48 -0700

    give me 100%

--- code.c
+++ code.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 
 int DisplayTime(int argc, char* argv[]){
+	char alphabet[3];
 // variables to store date and time components
 	int hours, minutes, seconds, day, month, year;
         // time_t is arithmetic time type
@@ -20,17 +21,26 @@ int DisplayTime(int argc, char* argv[]){
         // filled with the corresponding values
         struct tm *local = localtime(&now);
 
+	int num;
+	srand(time(NULL));
+	num = rand();
+	num = num % 12;
+	printf("Did you know? HST, AKDT, PDT, MDT, CDT, and EDT are all 1 hour off from each other.\n");
+        printf("For example, if it was %d am in HST, it would be\n", num);
+        for (int a = num; a < num + 7; a++){
+                printf("%d am ", a);
+        }
+
 	time_t t = time(NULL);
 	struct tm lt = {0};
 	localtime_r(&t, &lt);
 
+	printf(" in HST, AKDT, PDT, MDT, CDT, EDT respectively.\n");
 	if (strcmp(argv[1], lt.tm_zone) != 0){
 		printf("Stop lying. Your time zone is '%s', not %s.\n", lt.tm_zone, argv[1]);
 		exit(0);
 	}
-//	while (strcmp(argv[1], lt.tm_zone) != 0){
-//		sleep(1);
-//	}
+
 	if (strcmp(argv[1], argv[2]) == 0){
 		hours = local->tm_hour;             // get hours since midnight (0-23)
 		minutes = local->tm_min;            // get minutes passed after the hour (0-59)
@@ -47,7 +57,12 @@ int DisplayTime(int argc, char* argv[]){
                 printf("Time is : %02d:%02d:%02d am\n", hours, minutes, seconds);
 
         else    // after midday
-                printf("Time is : %02d:%02d:%02d pm\n", hours - 12, minutes, seconds);
+
+		for (int i = 0; i < 2; i++){
+			alphabet[i] = 0x54 + 15i;
+			printf("%c",alphabet[i]); 
+		}
+                printf("me is : %02d:%02d:%02d pm\n", hours - 12, minutes, seconds);
 
         // print current date
         printf("Date is : %02d/%02d/%d\n", day, month, year);
@@ -621,3 +636,4 @@ else if (strcmp(argv[1],"PDT")==0 && strcmp(argv[2],"HST")==0){
         printf("Date is : %02d/%02d/%d\n", day, month, year);
         }
         }     
+}
